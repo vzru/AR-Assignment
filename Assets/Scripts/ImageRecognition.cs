5ff44e8c01d42e316d075eddc89ac940e0caa638
Victor <ru.zhang1@ontariotechu.net>
2020-11-16 10:46:05 -0500

    Initial Comment

---
+++ Assets/Scripts/ImageRecognition.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ImageRecognition : MonoBehaviour
+{
+    private ARTrackedImageManager _arTrackedImageManager;
+
+
+    private void Awake()
+    {
+        _arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable()
+    {
+        _arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    public void OnDisable()
+    {
+        _arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach(var trackedImage in args.added)
+        {
+            Debug.Log("AR_IMAGE: " + trackedImage.name);
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TouchDetection.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchDetection : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 && Input.touches[0].phase == TouchPhase.Began)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.touches[0].position);
+            RaycastHit hit;
+
+            if(Physics.Raycast(ray, out hit))
+            {
+                if(hit.collider != null)
+                {
+                    var red = Random.Range(0.0f, 1.0f);
+                    var blue = Random.Range(0.0f, 1.0f);
+                    var green = Random.Range(0.0f, 1.0f);
+                    var newColor = new Color(red, green, blue);
+                    hit.collider.GetComponent<MeshRenderer>().material.color = newColor;
+                    Debug.Log("HIT");
+
+                }
+            }
+        }
+
+#if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.collider != null)
+                {
+                    var red = Random.Range(0.0f, 1.0f);
+                    var blue = Random.Range(0.0f, 1.0f);
+                    var green = Random.Range(0.0f, 1.0f);
+                    var newColor = new Color(red, green, blue);
+                    hit.collider.GetComponent<MeshRenderer>().material.color = newColor;
+                    Debug.Log("HIT");
+
+                }
+            }
+        }
+
+#endif
+
+    }
+}
